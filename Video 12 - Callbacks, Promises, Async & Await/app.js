3008bb0783edce2f205ca2103b836afabbeb5f40
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-08 14:32:25 -0400

    c

--- Video 12 - Callbacks, Promises, Async & Await/app.js
+++ Video 12 - Callbacks, Promises, Async & Await/app.js
@@ -1,8 +1,11 @@
+// This is asynchronous programming as the setTimeout function is called after 1 second and the console.log("4") is called before that. So, the output will be 1, 2, 3, 4, Hello.
+
 console.log("1");
 console.log("2");
 console.log("3");
-console.log("3");
 
 setTimeout(() => {
-  console.log("Hello");
+    console.log("Hello");
 }, 1000);
+
+console.log("4");
